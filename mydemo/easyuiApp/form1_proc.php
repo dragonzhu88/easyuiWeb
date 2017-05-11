84cdaf452feadefca2c775fbd1e1b723ebd264ad
dragon <dragonzhu1988@126.com>
2017-05-11 18:39:06 +0700

    launch

---
+++ mydemo/easyuiApp/form1_proc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dragon
+ * Date: 2017/4/3
+ * Time: 18:57
+ */
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
+echo "Your Name: $name <br/> Your Email: $email <br/> Your Phone: $phone";
---
+++ mydemo/easyuiApp/get_users.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dragon
+ * Date: 2017/4/3
+ * Time: 19:35
+ */
+
+$data = [
+    [
+        'firstname' => 'zdc',
+        'lastname' => 'zdc1',
+        'phone' => '123445',
+        'email' => 'zdc@qq.com'
+    ],
+    [
+        'firstname' => 'zdc',
+        'lastname' => 'zdc1',
+        'phone' => '123445',
+        'email' => 'zdc@qq.com'
+    ],
+    [
+        'firstname' => 'zdc',
+        'lastname' => 'zdc1',
+        'phone' => '123445',
+        'email' => 'zdc@qq.com'
+    ],
+    [
+        'firstname' => 'zdc',
+        'lastname' => 'zdc1',
+        'phone' => '123445',
+        'email' => 'zdc@qq.com'
+    ],
+    [
+        'firstname' => 'zdc',
+        'lastname' => 'zdc1',
+        'phone' => '123445',
+        'email' => 'zdc@qq.com'
+    ],
+
+];
+
+echo json_encode($data,JSON_UNESCAPED_UNICODE);
